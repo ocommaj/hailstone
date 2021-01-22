af4834e0bcf52df709a650edc144373746574c8f
ocommaj <sysadmin@xaviermicronesia.org>
2021-01-22 15:32:34 -0600

    configure dotenv plugin

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,9 @@ module.exports = env => {
       filename: './index.html',
       favicon: './src/assets/icons/favicon/fav.ico'
     }),
-    new Dotenv(),
+    new Dotenv({
+      systemvars: true
+    }),
   ],
   mode: process.env.NODE_ENV,
   }
