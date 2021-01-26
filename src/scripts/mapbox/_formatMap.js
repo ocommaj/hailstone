5a10704f64d75decb8dcf7d8e503eb8c7926d147
ocommaj <ocommaj@gmail.com>
2021-01-26 14:53:41 -0600

    Separate out map loader configs/methods, constrain zooms

---
+++ src/scripts/mapbox/_formatMap.js
@@ -0,0 +1,66 @@
+import SunCalc from 'suncalc';
+
+export const initialConfigs = {
+  zoom: 13.25,
+  center: [151.83, 7.427],
+  pitch: 75,
+  bearing: 125,
+  maxZoom: 16,
+  minZoom: 11,
+  style: 'mapbox://styles/jfo713/ckkddvzci04es17tej19qwxt7'
+}
+
+const Config = {
+  initial: initialConfigs,
+  render3D
+}
+
+export default Config;
+
+export function render3D(map) {
+  addTerrainSource(map);
+  addSkyLayer(map);
+}
+
+function addTerrainSource(map) {
+    map.addSource('mapbox-dem', {
+        type: 'raster-dem',
+        url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        tileSize: 512,
+        maxzoom: 16,
+        minzoom: 11,
+    })
+    map.setTerrain({ source: 'mapbox-dem', exaggeration: 1.5 });
+}
+
+function addSkyLayer(map) {
+  map.addLayer({
+         'id': 'sky',
+         'type': 'sky',
+         'paint': {
+           'sky-opacity': [
+             'interpolate',
+             ['linear'],
+             ['zoom'],
+             0,
+             0,
+             5,
+             0.3,
+             8,
+             1
+           ],
+          'sky-atmosphere-color': '#4589ff',
+          'sky-type': 'atmosphere',
+          'sky-atmosphere-sun': setSunPosition()
+         }
+     });
+
+  function setSunPosition() {
+    const { lat, lng } = map.getCenter()
+    const { sunrise } = SunCalc.getTimes( Date.now(), lat, lng );
+    const sunPos = SunCalc.getPosition(sunrise, lat, lng);
+    const sunAzimuth = 180 + (sunPos.azimuth * 180) / Math.PI;
+    const sunAltitude = 90 - (sunPos.altitude * 180) / Math.PI;
+    return [sunAzimuth, sunAltitude];
+  }
+}
--- src/scripts/mapbox/_loadMap.js
+++ src/scripts/mapbox/_loadMap.js
@@ -1,61 +1,14 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
-import SunCalc from 'suncalc';
+import { initialConfigs, render3D } from './_formatMap';
 import { restyleCursor, showMarkerPopup } from './_mapMarkerEvents';
 mapboxgl.accessToken = process.env.MB_TOKEN;
 
 export default function Map() {
   const container = document.getElementById('mainMap');
-  const map = new mapboxgl.Map({
-    container,
-    zoom: 13.25,
-    center: [151.83, 7.427],
-    pitch: 75,
-    bearing: 125,
-    style: 'mapbox://styles/jfo713/ckkddvzci04es17tej19qwxt7'
-  })
+  const map = new mapboxgl.Map({ container, ...initialConfigs })
 
-  map.on('load', function () {
-    map.addSource('mapbox-dem', {
-        type: 'raster-dem',
-        url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
-        tileSize: 512,
-        maxzoom: 14,
-    })
-
-    map.setTerrain({ source: 'mapbox-dem', exaggeration: 1.5 });
-
-    map.addLayer({
-           'id': 'sky',
-           'type': 'sky',
-           'paint': {
-             'sky-opacity': [
-               'interpolate',
-               ['linear'],
-               ['zoom'],
-               0,
-               0,
-               5,
-               0.3,
-               8,
-               1
-             ],
-            'sky-atmosphere-color': '#4589ff',
-            'sky-type': 'atmosphere',
-            'sky-atmosphere-sun': setSunPosition()
-           }
-       });
-  });
-
+  map.on('load', () => render3D(map));
   map.on('click', ({ point }) => showMarkerPopup(point, map))
   map.on('mousemove', ({ point }) => restyleCursor(point, map))
-
-  function setSunPosition() {
-    const { lat, lng } = map.getCenter()
-    const { sunrise } = SunCalc.getTimes( Date.now(), lat, lng );
-    const sunPos = SunCalc.getPosition(sunrise, lat, lng);
-    const sunAzimuth = 180 + (sunPos.azimuth * 180) / Math.PI;
-    const sunAltitude = 90 - (sunPos.altitude * 180) / Math.PI;
-    return [sunAzimuth, sunAltitude];
-  }
 
 }
