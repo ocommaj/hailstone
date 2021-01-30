dd7508ed584958e78b0bb0838a168963ff7374da
ocommaj <ocommaj@gmail.com>
2021-01-30 15:28:22 -0600

    clean out font imports, map const

--- src/scripts/mapbox/_loadMap.js
+++ src/scripts/mapbox/_loadMap.js
@@ -1,13 +1,13 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
-import { initialConfigs, render3D } from './_formatMap';
+import config from './_formatMap';
 import mapEvents from './_mapEvents';
 mapboxgl.accessToken = process.env.MB_TOKEN;
 
 export default function Map() {
   const container = document.getElementById('mainMap');
-  const map = new mapboxgl.Map({ container, ...initialConfigs });
+  const map = new mapboxgl.Map({ container, ...config.initial });
 
-  map.on('load', () => render3D(map));
+  map.on('load', () => config.render3D(map));
   map.on('click', ({ point }) => mapEvents.clickHandler(point, map));
   map.on('mousemove', ({ point }) => mapEvents.moveHandler(point, map));
 
