cdc02d79852f7f554bf4f37575883cdadfdac360
ocommaj <ocommaj@gmail.com>
2021-02-02 16:45:29 -0600

    add keyboard func to dropdown

--- src/scripts/modals/_customDropdown.js
+++ src/scripts/modals/_customDropdown.js
@@ -14,6 +14,7 @@ export default function CustomDropdown({
   const optionsList = document.createElement('ul');
 
   let selectedOption = formattedOptions.find(option => option.selected);
+  let selectedOptionIdx = formattedOptions.indexOf(selectedOption);
 
   topFormLabel.classList.add('fileUploadFormLabel');
   topFormLabel.id = `${id}Label`;
@@ -25,6 +26,26 @@ export default function CustomDropdown({
 
   dropdownWrapper.addEventListener('blur', () => {
     optionsList.classList.remove('show')
+  })
+
+  dropdownWrapper.addEventListener('keydown', (e) => {
+    switch (e.code) {
+      case "Space":
+        optionsList.classList.toggle('show');
+        break;
+      case "ArrowUp":
+        const prevOption = formattedOptions[selectedOptionIdx-1];
+        if (prevOption) selectValue(prevOption);
+        break;
+      case "ArrowDown":
+        const nextOption = formattedOptions[selectedOptionIdx+1];
+        if (nextOption) selectValue(nextOption);
+        break;
+      case "Enter":
+      case "Escape":
+        optionsList.classList.toggle('show');
+        break;
+    }
   })
 
   setupDropdown()
@@ -59,23 +80,15 @@ export default function CustomDropdown({
     })
   }
 
-  function selectValue(value) {
-    const newOption = formattedOptions.find(option => option.value === value);
-    const prevOption = selectedOption;
-    prevOption.selected = false;
-    prevOption.element.selected = false;
-    newOption.selected = true;
-    newOption.element.selected = true;
-    optionLabel.innerText = newOption.label;
-    selectedOption = newOption;
-  }
-
   function setupDropdown() {
     dropdownWrapper.tabIndex = 0;
     dropdownWrapper.classList.add('custom-select-container');
     optionLabel.classList.add('custom-select-value');
     optionsList.classList.add('custom-select-options');
     optionLabel.innerText = selectedOption.label;
+    if (optionLabel.innerText === '(Optional)') {
+      optionLabel.style.color = 'rgba(244,244,244,.7)'
+    }
     dropdownWrapper.appendChild(hiddenSelect);
     dropdownWrapper.appendChild(optionLabel);
     dropdownWrapper.appendChild(optionsList);
@@ -88,14 +101,34 @@ export default function CustomDropdown({
       optionElement.classList.toggle('selected', option.selected);
       optionElement.innerText = option.label;
       optionElement.dataset.value = option.value;
-      optionElement.addEventListener('click', () => {
-        const outgoing = optionsList.querySelector('.selected');
-        outgoing.classList.remove('selected');
-        selectValue(option.value);
-        optionElement.classList.add('selected')
-        optionsList.classList.remove('show')
-      });
+      optionElement.addEventListener('click', () => selectValue(option));
+      if (option.label === '(Optional)') {
+        optionElement.style.color = 'rgba(244,244,244,.7)'
+      }
       optionsList.appendChild(optionElement);
     })
+  }
+
+  function selectValue(newSelection) {
+    const outgoing = optionsList.querySelector('.selected');
+    const incoming = optionsList.querySelector(`
+      [data-value="${newSelection.value}"]
+    `);
+    outgoing.classList.remove('selected');
+    incoming.classList.add('selected');
+    optionsList.classList.remove('show');
+
+    const newOption = formattedOptions.find(option => {
+      return option.value === newSelection.value
+    });
+    const newIdx = formattedOptions.indexOf(newOption);
+    const prevOption = selectedOption;
+    prevOption.selected = false;
+    prevOption.element.selected = false;
+    newOption.selected = true;
+    newOption.element.selected = true;
+    optionLabel.innerText = newOption.label;
+    selectedOption = newOption;
+    selectedOptionIdx = newIdx;
   }
 }
